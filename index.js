df54c7d25186b5cf507dcec6e59581448ed025f6
GC <contact@gcteam.dev>
2022-06-23 00:22:56 -0700

    first commit estimator bot

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+var team1 = ['Sheila', 'Asia', 'Maxine', 'Christian','Getahun','Martin','Hamza',];
+console.log("This is JWD Team-1: "+ team1);
+
+  var candyPrice = 3;
+  var inventoryCount = 100;
+  var restock = 300 - inventoryCount;
+  var totalBeforeDelFee = restock * candyPrice;
+
+console.log("Last morning's total count of JWD-bites was: "+inventoryCount+";"+" We need total number of bag restock with JWD-bites are: = " + restock+";"+"total cost of bags before delivery fee will be: " + totalBeforeDelFee);
+
+
+if (restock <= 100) {
+  var delFee = 25;
+  console.log("Also, we would need 1 delivery person with the delivery fee of $25")
+} else if (restock <= 200){
+   var delFee = 50;
+   console.log("Also, we would need 2 delivery person with the delivery fee of $50")
+} else {
+   var delFee = 75;
+  console.log("And we would need 3 delivery person with the delivery fee of $75")
+}
+console.log( 'Have a “SWEET” day!');
+
+var total = totalBeforeDelFee + delFee;
+console.log("Total cost will be: " + total)
+*/
